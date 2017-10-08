de10869500218c30a69d7c5491298a789427b2d0
christiansegura <31946263+christiansegura@users.noreply.github.com>
2017-10-07 20:01:32 -0700

    Bot messages styled
    based on user input

--- bot.js
+++ bot.js
@@ -34,6 +34,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
             // Just add any case commands if you want to..
+            //review commands in api and begin to formulate responses
          }
      }
 });
